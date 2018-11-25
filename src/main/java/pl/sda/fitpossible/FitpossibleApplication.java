ac0273efc0f5611fde5a9a12f00be15e1b84d372
Jola <jolas774@gmail.com>
2018-11-25 19:43:18 +0100

    attach_local_repo

---
+++ src/main/java/pl/sda/fitpossible/FitpossibleApplication.java
@@ -0,0 +1,12 @@
+package pl.sda.fitpossible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitpossibleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitpossibleApplication.class, args);
+    }
+}
