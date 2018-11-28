a8f7d7e0843f55217c8dd14fd7803f7db60bd605
Jola <jolas774@gmail.com>
2018-11-28 23:42:58 +0100

    Activity_entity_dto

---
+++ src/main/java/pl/sda/fitpossible/dto/ActivityDto.java
@@ -0,0 +1,4 @@
+package pl.sda.fitpossible.dto;
+
+public class ActivityDto {
+}
---
+++ src/main/java/pl/sda/fitpossible/entity/ActivityType.java
@@ -0,0 +1,8 @@
+package pl.sda.fitpossible.entity;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    PUSHUPS,
+    SQUATS
+}
